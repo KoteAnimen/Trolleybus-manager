f75d0167b6ae620b0727320ded4bf35212512104
KoteAnimen <sverh.il@yandex.ru>
2021-05-12 15:55:03 +0300

    Добавьте файлы проекта.

---
+++ Trolleybus manager/MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Trolleybus_manager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ListStations = new System.Windows.Forms.Button();
+            this.Paths = new System.Windows.Forms.Button();
+            this.Trolleybusses = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Exit);
+            this.groupBox1.Controls.Add(this.Trolleybusses);
+            this.groupBox1.Controls.Add(this.Paths);
+            this.groupBox1.Controls.Add(this.ListStations);
+            this.groupBox1.Location = new System.Drawing.Point(15, 30);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Size = new System.Drawing.Size(353, 317);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Главное меню";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(385, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // ListStations
+            // 
+            this.ListStations.Location = new System.Drawing.Point(55, 48);
+            this.ListStations.Name = "ListStations";
+            this.ListStations.Size = new System.Drawing.Size(236, 39);
+            this.ListStations.TabIndex = 0;
+            this.ListStations.Text = "Справочник остановок";
+            this.ListStations.UseVisualStyleBackColor = true;
+            // 
+            // Paths
+            // 
+            this.Paths.Location = new System.Drawing.Point(55, 93);
+            this.Paths.Name = "Paths";
+            this.Paths.Size = new System.Drawing.Size(236, 39);
+            this.Paths.TabIndex = 1;
+            this.Paths.Text = "Маршруты";
+            this.Paths.UseVisualStyleBackColor = true;
+            // 
+            // Trolleybusses
+            // 
+            this.Trolleybusses.Location = new System.Drawing.Point(55, 138);
+            this.Trolleybusses.Name = "Trolleybusses";
+            this.Trolleybusses.Size = new System.Drawing.Size(236, 39);
+            this.Trolleybusses.TabIndex = 2;
+            this.Trolleybusses.Text = "Троллейбусы";
+            this.Trolleybusses.UseVisualStyleBackColor = true;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(55, 250);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(236, 39);
+            this.Exit.TabIndex = 3;
+            this.Exit.Text = "Выход";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 364);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Управление троллейбусами";
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Trolleybusses;
+        private System.Windows.Forms.Button Paths;
+        private System.Windows.Forms.Button ListStations;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+    }
+}
+
